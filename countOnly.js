2d28de46379060675ff1ebcd0b0a6a996ca1a7d1
Jade <jade.duong@telus.com>
2022-11-16 12:44:16 -0800

    adding finished countOnly

---
+++ countOnly.js
@@ -0,0 +1,9 @@
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item] === true) {
+      results[item] = results[item] ? results[item] + 1 : 1;
+    }
+  }
+  return results;
+};
