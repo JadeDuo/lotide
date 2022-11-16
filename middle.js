481dc853a16cae2b99c93dd26614157b9f9bc850
Jade <jade.duong@telus.com>
2022-11-16 10:54:10 -0800

    added middle.js

---
+++ middle.js
@@ -0,0 +1,13 @@
+const middle = function(arr) {
+  let midIndex = ((arr.length - 1) / 2);
+  if (arr.length <= 2) {
+    return [];
+  }
+  if (arr.length % 2 !== 0) {
+    return arr.slice(midIndex, midIndex + 1);
+  }
+  if (arr.length % 2 === 0) {
+    return arr.slice(midIndex, midIndex + 2);
+  }
+};
+
