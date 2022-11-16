f8a80108d599501c986fd8e7fc846a7e38ac2183
Jade <jade.duong@telus.com>
2022-11-16 15:04:17 -0800

    adding letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (!results[letter]) {
+      results[letter] = [];
+    }
+    results[letter].push(i);
+  }
+  return results;
+};
+
+
