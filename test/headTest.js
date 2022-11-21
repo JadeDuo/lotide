795fa4024926a195ec66cb1445c060684c80255d
Jade <jade.duong@telus.com>
2022-11-21 11:19:39 -0800

    updating test case for head to use mocha/chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require("../assertEqual");
-const head = require("../head");
+// const assertEqual = require("../assertEqual");
+// const head = require("../head");
 
-let arr = [1, 2, 3, 4];
-assertEqual(head(arr), 1);
+// let arr = [1, 2, 3, 4];
+// assertEqual(head(arr), 1);
 
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  ///
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
 
